fbfb84c21c2d56050054bb1498d6dc2ca4b83e76
Ana Sofia Henao <anahenao719@gmail.com>
2026-05-07 20:29:31 -0500

    Agregar modelo de tutor

---
+++ TutorMatch_Backend/Models/Tutor.cs
@@ -0,0 +1,17 @@
+﻿namespace TutorMatch_Backend.Models
+{
+    public class Tutor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Email { get; set; }
+    }
+}
+
+
+
+
