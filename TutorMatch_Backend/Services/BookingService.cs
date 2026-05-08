8edb4f3b347ac93550e3ff98b59a75970afa5dda
Ana Sofia Henao <anahenao719@gmail.com>
2026-05-07 21:15:28 -0500

    implementar lógica de reservas

---
+++ TutorMatch_Backend/Services/BookingService.cs
@@ -0,0 +1,40 @@
+﻿using TutorMatch_Backend.Models;
+
+namespace TutorMatch_Backend.Services
+{
+    public class BookingService
+    {
+        private static List<Booking> bookings = new();
+
+        public List<Booking> GetAll()
+        {
+            return bookings;
+        }
+
+        public Booking? GetById(int id)
+        {
+            return bookings.FirstOrDefault(b => b.Id == id);
+        }
+
+        public Booking Add(Booking booking)
+        {
+            booking.Id = bookings.Count + 1;
+
+            bookings.Add(booking);
+
+            return booking;
+        }
+
+        public bool Delete(int id)
+        {
+            var booking = GetById(id);
+
+            if (booking == null)
+                return false;
+
+            bookings.Remove(booking);
+
+            return true;
+        }
+    }
+}
