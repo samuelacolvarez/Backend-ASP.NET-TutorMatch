c6164b99ae3f6b39897726ebe3ab74713cb0c94e
Ana Sofia Henao <anahenao719@gmail.com>
2026-05-07 20:29:49 -0500

    agregar modelo de reserva

---
+++ TutorMatch_Backend/Models/Booking.cs
@@ -0,0 +1,15 @@
+﻿namespace TutorMatch_Backend.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        public string StudentName { get; set; }
+
+        public string Subject { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int TutorId { get; set; }
+    }
+}
