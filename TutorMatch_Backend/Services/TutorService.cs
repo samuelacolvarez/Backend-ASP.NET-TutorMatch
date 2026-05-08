f846568a2eaee644d6a996d9ab8fe511655c58e2
Ana Sofia Henao <anahenao719@gmail.com>
2026-05-07 21:15:17 -0500

    implementar lógica de tutores

---
+++ TutorMatch_Backend/Services/TutorService.cs
@@ -0,0 +1,40 @@
+﻿using TutorMatch_Backend.Models;
+
+namespace TutorMatch_Backend.Services
+{
+    public class TutorService
+    {
+        private static List<Tutor> tutors = new();
+
+        public List<Tutor> GetAll()
+        {
+            return tutors;
+        }
+
+        public Tutor? GetById(int id)
+        {
+            return tutors.FirstOrDefault(t => t.Id == id);
+        }
+
+        public Tutor Add(Tutor tutor)
+        {
+            tutor.Id = tutors.Count + 1;
+
+            tutors.Add(tutor);
+
+            return tutor;
+        }
+
+        public bool Delete(int id)
+        {
+            var tutor = GetById(id);
+
+            if (tutor == null)
+                return false;
+
+            tutors.Remove(tutor);
+
+            return true;
+        }
+    }
+}
