51d721af9828514196d32d40fbac47df819fa83d
Ana Sofia Henao <anahenao719@gmail.com>
2026-05-07 21:14:45 -0500

    implementar validaciones de reserva

---
+++ TutorMatch_Backend/Validators/BookingValidator.cs
@@ -0,0 +1,21 @@
+﻿using TutorMatch_Backend.DTOs;
+
+namespace TutorMatch_Backend.Validators
+{
+    public class BookingValidator
+    {
+        public static bool Validate(BookingDto booking)
+        {
+            if (string.IsNullOrWhiteSpace(booking.StudentName))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(booking.Subject))
+                return false;
+
+            if (booking.TutorId <= 0)
+                return false;
+
+            return true;
+        }
+    }
+}
