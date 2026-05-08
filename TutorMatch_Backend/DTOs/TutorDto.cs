286f2951456e40446e6e88dfa01f3df2da9230bf
Ana Sofia Henao <anahenao719@gmail.com>
2026-05-07 20:30:11 -0500

    agregar dto de tutor

---
+++ TutorMatch_Backend/DTOs/TutorDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TutorMatch_Backend.DTOs
+{
+    public class TutorDto
+    {
+        public string Name { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Email { get; set; }
+    }
+}
