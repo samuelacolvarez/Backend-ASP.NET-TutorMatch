ba3a8effac1be27e9ca85dc9035cf1e9ddf55c87
Ana Sofia Henao <anahenao719@gmail.com>
2026-05-07 20:30:25 -0500

    agregar dtos para tutores y reservas

---
+++ TutorMatch_Backend/DTOs/BookingDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TutorMatch_Backend.DTOs
+{
+    public class BookingDto
+    {
+        public string StudentName { get; set; }
+
+        public string Subject { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int TutorId { get; set; }
+    }
+}
