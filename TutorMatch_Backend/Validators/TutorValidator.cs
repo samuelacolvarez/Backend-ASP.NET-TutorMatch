fffa51cecd2484a318fa445b015e1e4b9d817ebd
Ana Sofia Henao <anahenao719@gmail.com>
2026-05-07 21:15:04 -0500

    implementar validaciones de tutor

---
+++ TutorMatch_Backend/Validators/TutorValidator.cs
@@ -0,0 +1,21 @@
+﻿using TutorMatch_Backend.DTOs;
+
+namespace TutorMatch_Backend.Validators
+{
+    public class TutorValidator
+    {
+        public static bool Validate(TutorDto tutor)
+        {
+            if (string.IsNullOrEmpty(tutor.Name))
+                return false;
+
+            if (string.IsNullOrEmpty(tutor.Subject))
+                return false;
+
+            if (string.IsNullOrEmpty(tutor.Email))
+                return false;
+
+            return true;
+        }
+    }
+}
