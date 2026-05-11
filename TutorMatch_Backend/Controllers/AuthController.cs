5a39e8ad654dce2594bca7f020a7fc1f685de819
Samuel Acosta <samuel.acosta28@eia.edu.co>
2026-05-10 21:33:24 -0500

    feat: infrastructure, auth, EF Core, JWT and seeder

---
+++ TutorMatch_Backend/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TutorMatch_Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController (IAuthenticationService authService) : ControllerBase
+{
+   
+   
+}
--- TutorMatch_Backend/Validators/Validators.cs
+++ TutorMatch_Backend/Validators/Validators.cs
@@ -1,6 +1,6 @@
 ﻿namespace TutorMatch_Backend.Validators
 {
-    public class Validator
+    public class RegisterValidators
     {
     }
 }
